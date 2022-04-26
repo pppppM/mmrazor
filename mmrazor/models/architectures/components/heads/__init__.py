008fdf3979b6176cea8b2a435e95ebc0c3f87cfe
gaojianfei <gaojianfei@sensetime.com>
2022-04-26 11:02:27 +0800

    add dropuout cls head

--- mmrazor/models/architectures/components/heads/__init__.py
+++ mmrazor/models/architectures/components/heads/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .darts_head import DartsSubnetClsHead
-
-__all__ = ['DartsSubnetClsHead']
+from .dropout_head import LinearDropoutClsHead
+__all__ = ['DartsSubnetClsHead', 'LinearDropoutClsHead']
